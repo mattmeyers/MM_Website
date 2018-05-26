bde57948ccf26b5492dfb2624b703b8323c3937e
Matt Meyers <mmeyers.07.95@gmail.com>
2018-05-25 23:14:38 -0400

    Added mcgpyutils and wsgi.py.

---
+++ mm_website/wsgi.py
@@ -0,0 +1,11 @@
+import sys
+import logging
+from mcgpyutils import FileSystemUtils
+
+logging.basicConfig(stream=sys.stderr)
+
+# Add the current directory to the python path.
+fsu = FileSystemUtils()
+sys.path.insert(0, fsu.get_path_to_script(__file__))
+
+from mm_website import app as application
