ba667e83a1f16f825317314edabfe4b03e73852f
Matt Meyers <mmeyers.07.95@gmail.com>
2018-06-10 21:34:31 -0400

    Removed files not being used anymore.

--- mm_website/mm_website.py
+++ mm_website/mm_website.py
@@ -13,18 +13,5 @@ def resume():
 def projects():
     return render_template('projects.html')
 
-@app.route('/euler')
-def euler():
-    return render_template('euler.html')
-
-@app.route('/math')
-def math():
-    return render_template('math.html')
-
-@app.route('/euler/<string:pe>')
-def eulerPE1(pe):
-    return render_template(f'ProjectEuler/{pe}/{pe}.html')
-
-
 if __name__ == '__main__':
     app.run(host="localhost", port=5000, debug=True)
