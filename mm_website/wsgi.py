66c62a217776607dc44309a699cfdc0f8660aa0e
Matt Meyers <mmeyers.07.95@gmail.com>
2018-06-24 16:28:08 -0400

    Fixed wsgi .env path.

--- mm_website/wsgi.py
+++ mm_website/wsgi.py
@@ -1,15 +1,17 @@
+import os
 import sys
 import logging
 from dotenv import load_dotenv
-from pathlib import path
 
-env_path = Path('.')/'../.env'
+path_to_file = os.path.dirname(os.path.abspath(__file__))
+
+env_path = f'{path_to_file}/../.env'
 load_dotenv(dotenv_path=env_path)
 
 logging.basicConfig(stream=sys.stderr)
 
 # Add the current directory to the python path.
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, path_to_file)
 
 from mm_website import create_app
 application = create_app()
